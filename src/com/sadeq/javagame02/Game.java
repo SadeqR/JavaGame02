08864548b1076b08d0694c72723ea0ece5b22f1a
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-02-15 22:01:31 +0000

    updated; Game.java; buffer strategy added

--- src/com/sadeq/javagame02/Game.java
+++ src/com/sadeq/javagame02/Game.java
@@ -2,6 +2,7 @@ package com.sadeq.javagame02;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -40,7 +41,20 @@ public class Game extends Canvas implements Runnable {
 
 	public void run() {
 		while (running) {
-			System.out.println("running...");
+			update();
+			render();
+		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
 		}
 	}
 	
