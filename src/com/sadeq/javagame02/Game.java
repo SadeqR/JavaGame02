f8801d844de2b29a98f6643f0d8fc54127ccf049
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-02-15 22:29:21 +0000

    updated; Game.java; buffered image created

--- src/com/sadeq/javagame02/Game.java
+++ src/com/sadeq/javagame02/Game.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -18,6 +20,13 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	//an image to draw things onto
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	//allows us to draw things onto the above image - to access the image
+	//getRaster gets the array of pixels that make the image
+	//getDataBuffer get the data buffer that handles the raster - data buffer we can modify
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
 	public Game() {
 		Dimension size = new Dimension(width*scale, height*scale);
