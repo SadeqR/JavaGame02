67a20e4f4fa2e758e060f98938ab4e9bdd844626
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-02-15 21:46:09 +0000

    updated; Game.java; window created

--- src/com/sadeq/javagame02/Game.java
+++ src/com/sadeq/javagame02/Game.java
@@ -1,12 +1,25 @@
 package com.sadeq.javagame02;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
 	public static int width = 300;
 	public static int height = width / 16* 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
+	
+	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 	
 	//synch. prevents thread interferences and memory inconsistency errors
 	public synchronized void start() {
@@ -27,7 +40,21 @@ public class Game implements Runnable {
 
 	public void run() {
 		while (running) {
-			
+			System.out.println("running...");
 		}
 	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("JavaGame02");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null); //centres window
+		game.frame.setVisible(true);
+		
+		game.start();
+	}
+	
 }
