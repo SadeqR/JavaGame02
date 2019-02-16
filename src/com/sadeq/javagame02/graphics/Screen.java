10ce4c50911358831655f5f156ecc8167ea26d09
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-02-16 19:34:38 +0000

    updated; Screen.java; random generated tiles added to the window/map

--- src/com/sadeq/javagame02/graphics/Screen.java
+++ src/com/sadeq/javagame02/graphics/Screen.java
@@ -1,15 +1,26 @@
 package com.sadeq.javagame02.graphics;
 
+import java.util.Random;
+
 public class Screen {
 
 	private int width;
 	private int height;
 	public int[] pixels;
 	
+	public int[] tiles = new int[64*64];
+	
+	private Random random = new Random();
+	
 	public Screen (int width, int height) {
 		 this.width = width;
 		 this.height = height;
 		 pixels = new int[width*height];
+		 
+		 for(int i = 0; i<64*64; i++) {
+			 tiles[i] = random.nextInt(0xffffff);
+		 }
+		 
 	}
 	
 	public void clear() {
@@ -20,8 +31,11 @@ public class Screen {
 	
 	public void render() {
 		for(int y=0; y < height; y++) {
+			if(y<0 || y>=height) break;
 			for(int x=0; x< width; x++) {
-				pixels[x + (y*width)] = 0xff00ff;
+				if(x<0 || x>= width) break;
+				int tileIndex = (x/16) + ((y/16) * 64);
+				pixels[x + (y*width)] = tiles[tileIndex];
 			}
 		}
 	}
