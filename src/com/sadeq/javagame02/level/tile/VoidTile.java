9f4fcc74d272313cbaa038707526192aa1f8e61b
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-03-05 23:17:44 +0000

    update; adding a void tile added; VoidTile.java

---
+++ src/com/sadeq/javagame02/level/tile/VoidTile.java
@@ -0,0 +1,16 @@
+package com.sadeq.javagame02.level.tile;
+
+import com.sadeq.javagame02.graphics.Screen;
+import com.sadeq.javagame02.graphics.Sprite;
+
+public class VoidTile extends Tile {
+
+	public VoidTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x, y, this);
+	}
+
+}
