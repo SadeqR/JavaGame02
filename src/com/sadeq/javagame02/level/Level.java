bdf91618976d5db778337be36713baabbe33ed90
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-03-10 22:09:33 +0000

    updated; getting ride of black edges

--- src/com/sadeq/javagame02/level/Level.java
+++ src/com/sadeq/javagame02/level/Level.java
@@ -36,9 +36,10 @@ public class Level {
 		screen.setOffset(xScroll, yScroll);
 		// >>4 same as /16; we do this to work with whole tiles not pixels
 		int x0 = xScroll >> 4;
-		int x1 = (xScroll + screen.width) >> 4;
+		// + 16 to get rid of black edges
+		int x1 = (xScroll + screen.width + 16) >> 4;
 		int y0 = yScroll >> 4;
-		int y1 = (yScroll + screen.height) >> 4;
+		int y1 = (yScroll + screen.height + 16) >> 4;
 		
 		for(int y = y0; y < y1; y++) {
 			for(int x = x0; x < x1; x++) {
