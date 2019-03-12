f9bab48cb76dc99e36f91d6e9e43fe505b2b76e6
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-03-12 22:19:02 +0000

    added; Mob.java; mob framework

---
+++ src/com/sadeq/javagame02/entity/mob/Mob.java
@@ -0,0 +1,24 @@
+package com.sadeq.javagame02.entity.mob;
+
+import com.sadeq.javagame02.entity.Entity;
+import com.sadeq.javagame02.graphics.Sprite;
+
+public abstract class Mob extends Entity {
+	
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+	
+	public void move() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	private boolean collision() {
+		return false;
+	}
+	
+}
