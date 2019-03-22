d1dd5f27b572dd361d87a2b1ec6422c6b911594f
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-03-22 21:11:44 +0000

    updated; animating fwd walk

--- src/com/sadeq/javagame02/graphics/Sprite.java
+++ src/com/sadeq/javagame02/graphics/Sprite.java
@@ -14,6 +14,9 @@ public class Sprite {
 	public static Sprite player_back = new Sprite(32, 2, 5, SpriteSheet.tiles);
 	public static Sprite player_side = new Sprite(32, 1, 5, SpriteSheet.tiles);
 	
+	public static Sprite player_forward_1 = new Sprite(32, 0, 6, SpriteSheet.tiles);
+	public static Sprite player_forward_2 = new Sprite(32, 0, 7, SpriteSheet.tiles);
+	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		SIZE=size;
 		pixels = new int[SIZE * SIZE];
