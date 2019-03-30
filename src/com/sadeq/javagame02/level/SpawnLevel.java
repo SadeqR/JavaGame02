2156c08233851bcf20154f5c6d1a0422789a739f
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-03-30 23:23:22 +0000

    updated; mapping tiles to the screen updated

--- src/com/sadeq/javagame02/level/SpawnLevel.java
+++ src/com/sadeq/javagame02/level/SpawnLevel.java
@@ -9,8 +9,6 @@ import com.sadeq.javagame02.level.tile.Tile;
 
 public class SpawnLevel extends Level {
 	
-	private int[] levelPixels;
-	
 	public SpawnLevel(String path) {
 		super(path);
 	}
@@ -18,11 +16,10 @@ public class SpawnLevel extends Level {
 	protected void loadLevel(String path) {
 		try {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
-			int w = image.getWidth();
-			int h = image.getHeight();
-			tiles = new Tile[w * h];
-			levelPixels = new int[w * h];
-			image.getRGB(0, 0, w, h, levelPixels, 0, w);
+			int w = width = image.getWidth();
+			int h = height = image.getHeight();			
+			tiles = new int[w * h];
+			image.getRGB(0, 0, w, h, tiles, 0, w);
 			generateLevel();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -30,14 +27,7 @@ public class SpawnLevel extends Level {
 		}
 	}
 	
-	// grass = 0xFF00
-	// flower = 0xFFFF00
-	// rock = 0x7F7F00
 	protected void generateLevel() {
-		for(int i = 0; i< levelPixels.length; i++) {
-			if(levelPixels[i] == 0xff00ff00) tiles[i] = Tile.grass;
-			if(levelPixels[i] == 0xffffff00) tiles[i] = Tile.flower;
-			if(levelPixels[i] == 0xff7f7f00) tiles[i] = Tile.rock;
-		}
+		System.out.println("Tiles: " + tiles[0]);
 	}
 }
