fb20debfd623e3f382976526d5698a656ec9608a
Sadeq <32779019+SadeqR@users.noreply.github.com>
2019-04-02 23:52:01 +0100

    spawn level assets continued;

---
+++ src/com/sadeq/javagame02/level/tile/spawn_level/SpawnGrassTile.java
@@ -0,0 +1,19 @@
+package com.sadeq.javagame02.level.tile.spawn_level;
+
+import com.sadeq.javagame02.graphics.Screen;
+import com.sadeq.javagame02.graphics.Sprite;
+import com.sadeq.javagame02.level.tile.Tile;
+
+public class SpawnGrassTile extends Tile {
+
+	public SpawnGrassTile(Sprite sprite) {
+		super(sprite);
+		
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		// converted to pixel precision
+		screen.renderTile(x<<4, y<<4, this);
+	}
+
+}
